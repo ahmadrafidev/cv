d630f31020762ed8770b924355d92fb9aa44d3f0
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-05-17 11:02:42 +0700

    fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
         <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">404</h1>
         <h2 className="text-2xl font-semibold tracking-tight">Page Not Found</h2>
         <p className="text-muted-foreground">
-          The page you're looking for doesn't exist or has been moved.
+          The page you are looking for does not exist or has been moved.
         </p>
       </div>
       <Link 
